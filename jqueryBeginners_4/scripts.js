5b28f52070201c3ca5830ade3c4f0a0b793a98a2
Suresh <suresh@pinnaclesoft.io>
2018-03-12 17:42:44 +0530

    jquery tutorial removed

--- jqueryBeginners_4/scripts.js
+++
@@ -1,3 +0,0 @@
-﻿$(function() {
-    $('li>a').addClass('alert');
-});
--- jquery_style_switcher/switcher.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$style = $_GET['style'];
-setCookie('style', $style, time() + 604800);
-header("location: " . $_SERVER['HTTP_REFERER']);
-
-?>
